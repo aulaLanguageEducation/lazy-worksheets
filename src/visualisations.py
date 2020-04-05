d16e8727aa9f6e100d0f2af0a66bba3999693eaf
unknown <alexgilbert3142@gmail.com>
2020-04-05 22:45:52 +0100

    working word cloud

--- src/visualisations.py
+++ src/visualisations.py
@@ -1,36 +1,47 @@
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-#from nltk.tokenize import word_tokenize
-
+from nltk.tokenize import word_tokenize
+from src import utils
 from nltk.corpus import stopwords
 
 
-def create_wordcloud(text: str):
+class Visualiser:
 
-    # This function generates a wordcloud given an input file and stop words
+    def __init__(self):
+        self.stop_words = set(stopwords.words('english'))
 
-    # Input:
-    # jsonFile - string, file name or topic (which a file name has just been created from)
+    def create_wordcloud(self, text: str):
 
-    # Inform the user that sometimes it takes awhile to produce a wordcloud
-    # so they don't think there is a problem
-    stop_words = set(stopwords.words('english'))
+        # This function generates a wordcloud given an input file and stop words
 
-    print("Generating word cloud (sometimes this takes a while)...")
+        # Input:
+        # jsonFile - string, file name or topic (which a file name has just been created from)
 
-    #word_tokens = word_tokenize(text)
 
-    word_tokens = text
+        print("Generating word cloud (sometimes this takes a while)...")
 
-    filtered_sentence = [w for w in word_tokens if w not in stop_words]
+        word_tokens = word_tokenize(text)
 
-    return filtered_sentence
-    """
-    wordcloud_por = WordCloud(background_color = "white", max_words = 10000).generate(text)
-    plt.figure(figsize = [27 ,7])
-    plt.imshow(wordcloud_por, interpolation = "bilinear")
-    plt.axis("off")
-    """
+        #word_tokens = text
+
+        filtered_word_list = [w for w in word_tokens if w not in self.stop_words]
+
+        filtered_words_str = ' '.join(filtered_word_list)
+
+        wordcloud_por = WordCloud(background_color="white", max_words=10000).generate(filtered_words_str)
+        plt.figure(figsize=[27, 7])
+        plt.imshow(wordcloud_por, interpolation="bilinear")
+        plt.axis("off")
+
+        #return filtered_sentence
+
+
 
 if __name__ == "__main__":
-    filtered_sentence = create_wordcloud('this is a big hippo that is called David')
+    TEST_URL_GUARDIAN = 'https://www.theguardian.com/uk-news/2019/dec/28/government-exposes-addresses-of-new-year-honours-recipients'
+
+    text_output = utils.get_body(TEST_URL_GUARDIAN)
+
+    this_visualiser = Visualiser()
+
+    this_visualiser.create_wordcloud(text_output)
