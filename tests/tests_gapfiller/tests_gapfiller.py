a1e206a1fad7b2163b2b125d0a733dcdc777a5bb
unknown <alexgilbert3142@gmail.com>
2020-04-18 16:33:14 +0100

    gap filler tests added

--- tests/tests_gapfiller/tests_gapfiller.py
+++ tests/tests_gapfiller/tests_gapfiller.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from src import gap_filler
 
 
 class TestPipeline(TestCase):
@@ -10,6 +11,18 @@ class TestPipeline(TestCase):
         actual_output = input_data**2
 
         expected_output = 4
+
+        self.assertEqual(expected_output, actual_output)
+
+    def test_example__divide(self):
+
+        input_data = 'this is a string with the noun dog'
+
+        this_gapfiller = gap_filler.GapFinder()
+
+        actual_output = this_gapfiller.find_gaps(input_data)
+
+        expected_output = 'this is a string with the noun _______'
 
         self.assertEqual(expected_output, actual_output)
 
