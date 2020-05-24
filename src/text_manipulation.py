4d4c81c994bbdb855f66d5db5c19be565b567deb
Hibbs-13 <paulhibbert88@gmail.com>
2020-05-24 09:55:47 +0100

    updated_manipulator_for_comments

--- src/text_manipulation.py
+++ src/text_manipulation.py
@@ -40,46 +40,60 @@ class Manipulator:
 
      #return sentences_list
 
-    def reorder_sentences(self, sentences_list):
+
+    def find_spelling_mistake(self, sentences_list):
+       # Selects the first sentence in the paragraph and converts it into string
+        first_sentence = str(sentences_list[0])
+        print(first_sentence)
+       # Splits the first sentence into words
+        words_in_first_sentence = first_sentence.split()
+       # Converts fist sentence into a list
+        list_of_first_sentence = list(words_in_first_sentence)
+        print(words_in_first_sentence)
+        print("----------")
+        for x in sentences_list:
+            str(x)
+            print(x)
+        print(len(x))
+
+
+   # def reorder_sentences(self, sentences_list):
         # the below function takes a random sentence in a paragraph and replaces it a random different point
 
         # the line below finds the length of the paragraph
-        length_of_paragraph = len(sentences_list)
+       # length_of_paragraph = len(sentences_list)
 
         # the line below creates a copy of the original list of text
-        copy_sentences_list = sentences_list.copy()
+       # copy_sentences_list = sentences_list.copy()
 
-        # the below takes a sentence from the length of the paragraph list
-        rand_idx = random.randrange(length_of_paragraph)
-        random_sentence = sentences_list[rand_idx]
+        # the below takes a random sentence from the length of the paragraph list
+       # rand_idx = random.randrange(length_of_paragraph)
+       # random_sentence = sentences_list[rand_idx]
 
         # the line below takes random sentence and indexes it to show the position of the random sentence was within the paragraph, it calls this variable 'index_random_sentence'
-        index_random_sentence = sentences_list.index(random_sentence)
+       # index_random_sentence = sentences_list.index(random_sentence)
 
         # the line below takes the copy of the original paragraph and removes the random sentence, creating a new shorter list
-        copy_sentences_list.remove(random_sentence)
+       # copy_sentences_list.remove(random_sentence)
 
-        # the below is creating a copy of the index ot the random sentence and calling it 'copy_index_random_sentence'
-        copy_index_random_sentence = index_random_sentence
+        # the below is creating a copy of the index of the random sentence and calling it 'copy_index_random_sentence'
+       # copy_index_random_sentence = index_random_sentence
 
         # the below then ensures the new copy of the index of the random sentence is a different number to the old index (limited to the size of the paragraph)
-        while copy_index_random_sentence == index_random_sentence:
-            copy_index_random_sentence = random.randrange(length_of_paragraph)
+       # while copy_index_random_sentence == index_random_sentence:
+       #     copy_index_random_sentence = random.randrange(length_of_paragraph)
 
-        # the below creates a new list which takes the paragraph with the sentence removed, then inserts the random sentence at a random place (not in it's original position)
-        jumbled_sentences_list = copy_sentences_list
-        jumbled_sentences_list.insert(copy_index_random_sentence,random_sentence)
-        print(jumbled_sentences_list)
+      #  copy_sentences_list.insert(copy_index_random_sentence,random_sentence)
+      #  print(copy_sentences_list)
 
         # the below gives the answers
-        print("--------------------------------------------------------------------------------------------------")
-        print("ANSWERS")
-        print("The sentence in the incorrect place was:")
-        print(random_sentence)
-        print("This should have been sentence number:")
-        print(index_random_sentence+1)
-        print("The paragraph reads:")
-        print(sentences_list)
+      #  print("--------------------------------------------------------------------------------------------------")
+      #  print("ANSWERS")
+      #  print("The sentence in the incorrect place was: " + random_sentence)
+      #  correct_sentence_number = str(index_random_sentence+1)
+      #  print("This should have been sentence number: " + correct_sentence_number)
+      #  print("The paragraph should read:")
+      #  print(sentences_list)
 
 
 
@@ -104,7 +118,7 @@ def main():
     #article_text = utils.get_body(TEST_URL_GUARDIAN)
     article_text = "Good morning, are you aware? Gilbert is addicted to chocolate. It's not great for him. He should eat less chocolate, but he won't."
     sentences_list = utils.get_sentences(article_text)
-    output_sentences = this_manipulator.reorder_sentences(sentences_list)
+    output_sentences = this_manipulator.find_spelling_mistake(sentences_list)
     return output_sentences
 
 if __name__ == "__main__":
