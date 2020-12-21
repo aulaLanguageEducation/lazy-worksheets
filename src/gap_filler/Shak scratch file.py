b8e9bc3c1e86cabeae1f5bc40d58fcc5b4caf643
Shak <76212423+Shak-Welch@users.noreply.github.com>
2020-12-21 22:58:12 +0000

    initial commit

---
+++ src/gap_filler/Shak scratch file.py
@@ -0,0 +1,11 @@
+
+#variables - the task
+
+sentence_1 = ("I am in the park.")
+sentence_2 = ("in the park.")
+sentence_3 = ("it was in.")
+
+print(sentence_1.replace("in","- -")),
+print(sentence_2.replace("in","- -")),
+print(sentence_3.replace("in","- -"))
+
