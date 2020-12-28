4017cbaf133d78e1d61f40601d7ebc465f549664
unknown <alexgilbert3142@gmail.com>
2020-12-28 15:54:48 +0000

    pushing to change branch

---
+++ src/dev/sandbox.py
@@ -0,0 +1,9 @@
+
+sentence_1 = ['I', 'am', 'in', 'the', 'park.']
+
+sentence_2 = ['In', 'the', 'park.']
+
+sentence_3 = ['It', 'was', 'in.']
+
+
+expected_result_1 = ['I', 'am', '__', 'the', 'park.']
